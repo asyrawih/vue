afb36b05d62dad9ea249509fe86e05a8577efcfe
hanan <hasyrawi@gmail.com>
2018-09-12 07:56:10 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+        <router-view/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Api.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div v-for="item in data" :key="item.id">
+                {{ item.id}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import VueResource from "vue-resource";
+export default {
+  data() {
+    return {
+      data: [""]
+    };
+  },
+  created() {
+    this.$http
+      .get(`https://jsonplaceholder.typicode.com/posts`)
+      .then(result => {
+        console.log(result.body.id);
+        this.data = result.data.title;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+};
+</script>
+
